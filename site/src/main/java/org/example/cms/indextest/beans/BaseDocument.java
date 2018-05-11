7bbed454329909706468e892f0a0fc5ccce5d741
Woonsan Ko <woonsan.ko@bloomreach.com>
2018-05-11 08:28:32 -0400

    Initial project creation

---
+++ site/src/main/java/org/example/cms/indextest/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.example.cms.indextest.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="myindextestproject:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
